0a2308b882b44e9f2d8e5ce0f1b052aed33bda5a
pwolle <ihaveunitdeterminant@gmail.com>
2024-09-30 14:09:56 +0200

    einops wrapper

--- flarejax/net/__init__.py
+++ flarejax/net/__init__.py
@@ -4,6 +4,7 @@ Neural network components.
 
 from ._attention import CrossAttention, DotProductAttention
 from ._combine import Add, Constant, Multiply, Sequential
+from ._einops import Einsum, Rearrange, Reduce, Repeat
 from ._linear import Bias, Linear, Scale
 
 __all__ = [
@@ -13,6 +14,10 @@ __all__ = [
     "Constant",
     "Multiply",
     "Sequential",
+    "Einsum",
+    "Rearrange",
+    "Reduce",
+    "Repeat",
     "Bias",
     "Linear",
     "Scale",
---
+++ flarejax/net/_einops.py
@@ -0,0 +1,120 @@
+"""
+Wrapper around the populer einops library.
+"""
+
+from typing import Literal
+
+import einops
+
+from .._module import Module
+
+
+__all__ = [
+    "Rearrange",
+    "Reduce",
+    "Repeat",
+    "Einsum",
+]
+
+
+class Rearrange(Module):
+    """
+    Rearrange the input tensor according to the given pattern.
+    See `einops.rearrange` for more information.
+
+    Parameters
+    ---
+    pattern: str
+        The pattern to rearrange the input tensor.
+
+    dims: int
+        The dimensions to use in the pattern.
+    """
+
+    def __init__(self, pattern: str, **dims: int) -> None:
+        self.pattern = pattern
+        self.dims = dims
+
+    def __call__(self, *args):
+        return einops.rearrange(*args, pattern=self.pattern, **self.dims)
+
+
+class Reduce(Module):
+    """
+    Reduce the input tensor according to the given pattern and the reduction
+    type. See `einops.reduce` for more information.
+
+    Parameters
+    ---
+
+    pattern: str
+        The pattern to reduce the input tensor.
+
+    reduction: str
+        The reduction type to use. Can be one of "sum", "mean", "max", "min",
+        or "prod".
+
+    dims: int
+        The dimensions to use in the pattern.
+    """
+
+    def __init__(
+        self,
+        pattern: str,
+        reduction: Literal["sum", "mean", "max", "min", "prod"],
+        **dims,
+    ) -> None:
+        self.pattern = pattern
+        self.reduction = reduction
+        self.dims = dims
+
+    def __call__(self, *args):
+        return einops.reduce(
+            *args,
+            pattern=self.pattern,
+            reduction=self.reduction,
+            **self.dims,
+        )
+
+
+class Repeat(Module):
+    """
+    Repeat the input tensor according to the given pattern. See `einops.repeat`
+    for more information.
+
+    Parameters
+    ---
+    pattern: str
+        The pattern to repeat the input tensor.
+
+    dims: int
+        The dimensions to use in the pattern.
+    """
+
+    def __init__(self, pattern: str, **dims: int) -> None:
+        self.pattern = pattern
+        self.dims = dims
+
+    def __call__(self, *args):
+        return einops.repeat(*args, pattern=self.pattern, **self.dims)
+
+
+class Einsum(Module):
+    """
+    Wrapper around `einops.einsum`. Performs a contraction on the input tensors
+    according to the given pattern.
+
+    Parameters
+    ---
+    pattern: str
+        The pattern to contract the input tensors.
+
+    dims: int
+        The dimensions to use in the pattern.
+    """
+
+    def __init__(self, pattern: str) -> None:
+        self.pattern = pattern
+
+    def __call__(self, *args):
+        return einops.einsum(*args, pattern=self.pattern)  # type: ignore
